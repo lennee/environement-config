6b5c7f01680bd03c917c80ca3d5c85b9888006db
Tristan Smith <Tristan Smith>
2021-07-25 05:13:20 -0400

    updated linux side of implementation

--- installs/install.sh
+++ installs/install.sh
@@ -8,7 +8,7 @@ install_node_packages_and_yarn(){
     export NVM_DIR=~/.nvm
     source $(brew --prefix nvm)/nvm.sh' >> ~/.zshrc
 
-  source ~/.zshrc
+  source $1/.zshrc
 
   # Install node
   nvm install 16
@@ -41,13 +41,16 @@ if [[ "`uname`" == "Linux" ]]; then
   pacman -S code etcher nvm postman-bin pyenv slack-desktop julia tmux via-bin
 
   pamac build spotify
+  HOME
+  HOME_DIR="/home/tristan"
 elif [[ "`uname`" == "Darwin" ]]; then
   # Installing programs
   brew install htop julia nvm pyenv tmux wget
 
   # Installing Cask Software
   brew install --cask ableton-live-suite balenaetcher firefox postman slack spotify via visual-studio-code
+  HOME_DIR="~"
 fi
 
-install_python_packages
+install_python_packages ${HOME_DIR}
 install_node_packages_and_yarn
